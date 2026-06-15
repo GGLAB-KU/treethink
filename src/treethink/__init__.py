fbb54c982672ccdb1fed14d4dc38180a8eb572a2
Can <147329006+erer-can@users.noreply.github.com>
2026-06-15 21:14:33 +0300

    chore: export RMaxTS evaluators

--- src/treethink/__init__.py
+++ src/treethink/__init__.py
@@ -22,6 +22,7 @@ from treethink.async_evaluators import (
     AsyncJudgeEvaluator,
     AsyncLeanREPLEvaluator,
     AsyncNormLenEvaluator,
+    AsyncRMaxTSEvaluator,
     get_async_evaluator_from_config,
 )
 from treethink.async_policies import (
@@ -110,6 +111,7 @@ from treethink.evaluators import (
     NormLenEvaluator,
     NormLenProbEvaluator,
     ProbEvaluator,
+    RMaxTSEvaluator,
     RocqEvaluator,
     TournamentEvaluator,
     get_evaluator,
@@ -206,6 +208,7 @@ __all__ = [
     "AsyncLeanClientAdapter",
     "AsyncLeanREPLEvaluator",
     "AsyncNormLenEvaluator",
+    "AsyncRMaxTSEvaluator",
     "AsyncPolicyType",
     "AsyncProofAssistantClient",
     "AsyncVLLMPolicy",
@@ -250,6 +253,7 @@ __all__ = [
     "ProbEvaluator",
     "ProofAssistantClient",
     "ProofCache",
+    "RMaxTSEvaluator",
     "ReplRuntime",
     "RocqBatchClient",
     "RocqEvaluator",
